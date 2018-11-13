9207402491a4943b386dc11578635eaf75e62b73
임형묵 <skygudanr@naver.com>
2018-11-13 15:56:32 +0900

    Add files via upload
    - 랜덤 함수 추가

--- helper.js
+++ helper.js
@@ -244,5 +244,24 @@ function Helper() {
         return array;
     }
 
+    // 0 (포함) and 1 (불포함) 난수를 반환
+    cls.getRandom = function() {
+        return Math.random();
+    }
+    // min (포함) 과 max (불포함) 사이의 난수를 반환
+    cls.getRandomArbitrary = function(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    // min (포함) 과 max (불포함) 사이의 임의 정수를 반환
+    // Math.round() 를 사용하면 고르지 않은 분포를 얻게된다!
+    cls.getRandomInt = function(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    // min (포함) 과 max (포함) 사이의 임의 정수를 반환
+    // Math.round() 를 사용하면 고르지 않은 분포를 얻게된다!
+    cls.getRandomIntInclusive = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     cls.polyfill();
 }
